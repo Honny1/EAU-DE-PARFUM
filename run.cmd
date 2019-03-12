a8b793ed6cacacf6bf1efb9cdadf9b63f0fea180
Jan Rodák <hony.com@seznam.cz>
2019-03-12 14:13:28 +0100

    Fix start on windows

--- run.cmd
+++ run.cmd
@@ -1,7 +1,8 @@
 echo@off
 del ./db/mongodb.lock
+timeout 1
 start mongodb/bin/mongod.exe --repair --dbpath="./db"
+timeout 5
 start mongodb/bin/mongod.exe --dbpath="./db"
 start index-win.exe
 echo "server and database run!!!"
-
